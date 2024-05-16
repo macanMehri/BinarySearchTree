a9c7d4bf27047453208d1669aa99d0ba07b82dea
Macan Mehri <macanmehri@gmail.com>
2024-05-16 22:18:19 +0330

    Created binary search tree class and its node class and added a method to insert new node to the tree

---
+++ binary_search_tree.py
@@ -0,0 +1,58 @@
+from typing import Any
+
+
+class Node:
+    """A class for BST tree nodes"""
+    def __init__(self, data: Any, left_child, right_child, parent):
+        self.data = data
+        self.left_child = left_child
+        self.right_child = right_child
+        self.parent = parent
+
+    def __str__(self) -> str:
+        return f'({self.data})'
+
+
+class BinarySearchTree:
+    """
+    A class for binary search tree or BST.
+    BST is a data structure which is not a complete binary tree.
+    The right child is greater than the parent and the parent is greater than the left child.
+    Uses linked lists to store data.
+    Use this data structure for searching or sorting.
+    """
+    def __init__(self, root_data: Any):
+        self.root = Node(
+            data=root_data,
+            left_child=None,
+            right_child=None,
+            parent=None,
+        )
+
+    def insert_node(self, data: Any) -> None:
+        """Insert new node to the tree in the right place"""
+        z = Node(
+            data=data,
+            left_child=None,
+            right_child=None,
+            parent=None,
+        )
+        y = None
+        x = self.root
+        while x is not None:
+            y = x
+            if z.data < x.data:
+                x = x.left_child
+            else:
+                x = x.right_child
+        z.parent = y
+        if y is None:
+            self.root = z
+        elif z.data < y.data:
+            y.left_child = z
+        else:
+            y.right_child = z
+
+
+if __name__ == '__main__':
+    pass
