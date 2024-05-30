da61fa1a0f5f8159e69e3f3915d6e4c595b686da
Macan Mehri <macanmehri@gmail.com>
2024-05-30 18:52:59 +0330

    Created constants file for constants variables

---
+++ constants.py
@@ -0,0 +1,6 @@
+MENU = (
+    '1. Create a max heap.\n'
+    '2. Create a binary search tree.\n'
+    '3. Add some highest values of binary search tree to max heap.\n'
+    '-------------------------------------------------------------'
+)
