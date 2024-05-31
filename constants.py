cd2efb06d1376a1bffb6dd4d2f14ba6d7676e095
Macan Mehri <macanmehri@gmail.com>
2024-05-31 19:20:10 +0330

    Completed the menu

--- constants.py
+++ constants.py
@@ -1,6 +1,11 @@
 MENU = (
+    '\n-------------------------------------------------------------\n'
     '1. Create a max heap.\n'
     '2. Create a binary search tree.\n'
     '3. Add some highest values of binary search tree to max heap.\n'
-    '-------------------------------------------------------------'
+    '4. Show maximum value of binary search tree.\n'
+    '5. Show minimum value of binary search tree.\n'
+    '6. Inorder tree walk for binary search tree.\n'
+    '0. Exit.'
+    '\n-------------------------------------------------------------\n'
 )
